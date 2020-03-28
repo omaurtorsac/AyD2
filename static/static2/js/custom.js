d2d489fb885a45648f866e39aa6b78512a2c3839
Mauro Herrera <mau.alejo.castro@gmail.com>
2020-03-27 21:50:05 -0600

    Productos

--- static/static2/js/custom.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by mauro on 31/12/17.
- */
-function nobackbutton(){
-    window.location.hash="no-back-button";
-    window.location.hash="Again-No-back-button"
-    window.onhashchange=function(){window.location.hash="no-back-button";}
-}
